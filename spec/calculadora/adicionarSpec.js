73e1fdd50e24a531469d660bd9653cc4152f9c3d
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-08-01 22:28:28 -0300

    Added a function to sum numbers

---
+++ spec/calculadora/adicionarSpec.js
@@ -0,0 +1,24 @@
+describe('Suite de teste de adição', () => {
+	let Calculadora = require('../../src/js/calculadora');
+
+	it('deve retornar 5 para 2 e 3', () => {
+		expect(Calculadora.adicionar(2,3)).toEqual(5);
+	})
+
+	it('deve retornar 6 para 9 e -3 no formato texto', () => {
+		expect(Calculadora.adicionar('9', '-3')).toEqual(6);
+	})
+
+	it('deve retornar 4.5 para 1.5 e 3', () => {
+		expect(Calculadora.adicionar(1.5,3)).toEqual(4.5);
+	});
+
+	it('deve retornar 0 quando o valor 1 não for numérico', () => {
+		expect(Calculadora.adicionar(undefined, 5)).toEqual(0);
+	});
+
+	it('deve retornar 0 quando o valor 2 não for numérico', () => {
+		expect(Calculadora.adicionar(5, undefined)).toEqual(0);
+	});
+
+})
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,13 +1,19 @@
 let Calculadora = {
 	adicionar: (num1, num2) => {
-		return 0;
+		if (isNaN(num1) || isNaN(num2)) {
+			return 0;
+		}
+		return (+num1) + (+num2);
 	},
+
 	subtrair: (num1, num2) => {
 		return 0;
 	},
+
 	dividir: (num1, num2) => {
 		return 0;
 	},
+
 	multiplicar: (num1, num2) => {
 		return 0;
 	},
